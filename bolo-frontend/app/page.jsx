bcedae66df1cdf90581986601fab592625f46452
Juan Martín Román <maestroxd4@gmail.com>
2024-11-14 19:29:32 -0300

    DBL-0

--- bolo-frontend/app/page.jsx
+++ bolo-frontend/app/page.jsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div>
       <div>
-      <Image
-        src={imagen1}
-        alt="Imagen de fondo"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-      />        
+        <Image
+          src={imagen1}
+          alt="Imagen de fondo"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />        
         <div className="relative z-10">
           <h1 className="text-white text-4xl tracking-widest p-2 font-texto">BOLO</h1>
         </div>
